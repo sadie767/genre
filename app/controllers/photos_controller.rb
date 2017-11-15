bab36db16ab58cba3287e16e50fc39721bf57f20
rm <biffbangpow767@yahoo.com>
2017-11-15 13:59:00 -0800

    demonstrate CRUD add the ability to create, read, update, and delete photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,9 +20,28 @@ class PhotosController < ApplicationController
     end
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to photos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path
+  end
+
   private
   def photo_params
     params.require(:photo).permit(:url)
   end
-  
+
 end
